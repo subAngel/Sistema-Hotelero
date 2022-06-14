c38cfb8cc0a7f32b1e9abae0f34f93d99f807a10
subAngel <whoangel.agl@gmail.com>
2022-06-14 00:53:15 -0500

    Botones de habitacion

--- src/GUI/FrameRecepcionista.java
+++ src/GUI/FrameRecepcionista.java
@@ -27,34 +27,232 @@ public class FrameRecepcionista extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jPanel3 = new javax.swing.JPanel();
+        panel_habitaciones = new javax.swing.JPanel();
+        jPanel4 = new javax.swing.JPanel();
+        jPanel2 = new javax.swing.JPanel();
+        jButton1 = new javax.swing.JButton();
+        jButton2 = new javax.swing.JButton();
+        jButton3 = new javax.swing.JButton();
+        jButton9 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+        jButton13 = new javax.swing.JButton();
+        jButton14 = new javax.swing.JButton();
+        jButton10 = new javax.swing.JButton();
+        jButton7 = new javax.swing.JButton();
+        jButton8 = new javax.swing.JButton();
+        jButton11 = new javax.swing.JButton();
+        jButton4 = new javax.swing.JButton();
+        jButton21 = new javax.swing.JButton();
+        jButton6 = new javax.swing.JButton();
+        jButton12 = new javax.swing.JButton();
+        jButton15 = new javax.swing.JButton();
+        jButton16 = new javax.swing.JButton();
+        jButton17 = new javax.swing.JButton();
+        H19 = new javax.swing.JButton();
+        jButton19 = new javax.swing.JButton();
+        jButton20 = new javax.swing.JButton();
+        jButton22 = new javax.swing.JButton();
+        jButton23 = new javax.swing.JButton();
+        jButton24 = new javax.swing.JButton();
+        jButton25 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
-        jPanel1.setBackground(new java.awt.Color(68, 71, 90));
+        jPanel1.setBackground(new java.awt.Color(40, 42, 54));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
-
-        jPanel2.setBackground(new java.awt.Color(98, 114, 164));
-
-        javax.swing.GroupLayout jPanel2Layout = new javax.swing.GroupLayout(jPanel2);
-        jPanel2.setLayout(jPanel2Layout);
-        jPanel2Layout.setHorizontalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 1000, Short.MAX_VALUE)
-        );
-        jPanel2Layout.setVerticalGroup(
-            jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 600, Short.MAX_VALUE)
-        );
-
-        jPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 90, 1000, 600));
 
         jLabel1.setFont(new java.awt.Font("Roboto Black", 3, 48)); // NOI18N
         jLabel1.setForeground(new java.awt.Color(248, 248, 242));
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("RECEPCIONISTA");
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 20, 410, 60));
+
+        jPanel3.setBackground(new java.awt.Color(98, 114, 164));
+        jPanel3.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        javax.swing.GroupLayout panel_habitacionesLayout = new javax.swing.GroupLayout(panel_habitaciones);
+        panel_habitaciones.setLayout(panel_habitacionesLayout);
+        panel_habitacionesLayout.setHorizontalGroup(
+            panel_habitacionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 298, Short.MAX_VALUE)
+        );
+        panel_habitacionesLayout.setVerticalGroup(
+            panel_habitacionesLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 450, Short.MAX_VALUE)
+        );
+
+        jPanel3.add(panel_habitaciones, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 123, -1, 450));
+
+        jPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 100, 360, 670));
+
+        jPanel4.setBackground(new java.awt.Color(68, 71, 90));
+        jPanel4.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
+
+        jPanel2.setBackground(new java.awt.Color(68, 71, 90));
+        jPanel2.setLayout(new java.awt.GridLayout(5, 5, 15, 15));
+
+        jButton1.setBackground(new java.awt.Color(139, 233, 253));
+        jButton1.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(68, 71, 90));
+        jButton1.setText("H1");
+        jButton1.setMaximumSize(new java.awt.Dimension(100, 100));
+        jButton1.setMinimumSize(new java.awt.Dimension(120, 120));
+        jPanel2.add(jButton1);
+
+        jButton2.setBackground(new java.awt.Color(139, 233, 253));
+        jButton2.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton2.setForeground(new java.awt.Color(68, 71, 90));
+        jButton2.setText("H2");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton2);
+
+        jButton3.setBackground(new java.awt.Color(139, 233, 253));
+        jButton3.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton3.setForeground(new java.awt.Color(68, 71, 90));
+        jButton3.setText("H3");
+        jPanel2.add(jButton3);
+
+        jButton9.setBackground(new java.awt.Color(139, 233, 253));
+        jButton9.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton9.setForeground(new java.awt.Color(68, 71, 90));
+        jButton9.setText("H4");
+        jPanel2.add(jButton9);
+
+        jButton5.setBackground(new java.awt.Color(139, 233, 253));
+        jButton5.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton5.setForeground(new java.awt.Color(68, 71, 90));
+        jButton5.setText("H5");
+        jPanel2.add(jButton5);
+
+        jButton13.setBackground(new java.awt.Color(139, 233, 253));
+        jButton13.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton13.setForeground(new java.awt.Color(68, 71, 90));
+        jButton13.setText("H6");
+        jPanel2.add(jButton13);
+
+        jButton14.setBackground(new java.awt.Color(139, 233, 253));
+        jButton14.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton14.setForeground(new java.awt.Color(68, 71, 90));
+        jButton14.setText("H7");
+        jPanel2.add(jButton14);
+
+        jButton10.setBackground(new java.awt.Color(139, 233, 253));
+        jButton10.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton10.setForeground(new java.awt.Color(68, 71, 90));
+        jButton10.setText("H8");
+        jPanel2.add(jButton10);
+
+        jButton7.setBackground(new java.awt.Color(139, 233, 253));
+        jButton7.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton7.setForeground(new java.awt.Color(68, 71, 90));
+        jButton7.setText("H9");
+        jPanel2.add(jButton7);
+
+        jButton8.setBackground(new java.awt.Color(139, 233, 253));
+        jButton8.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton8.setForeground(new java.awt.Color(68, 71, 90));
+        jButton8.setText("H10");
+        jPanel2.add(jButton8);
+
+        jButton11.setBackground(new java.awt.Color(139, 233, 253));
+        jButton11.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton11.setForeground(new java.awt.Color(68, 71, 90));
+        jButton11.setText("H11");
+        jPanel2.add(jButton11);
+
+        jButton4.setBackground(new java.awt.Color(139, 233, 253));
+        jButton4.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton4.setForeground(new java.awt.Color(68, 71, 90));
+        jButton4.setText("H12");
+        jPanel2.add(jButton4);
+
+        jButton21.setBackground(new java.awt.Color(139, 233, 253));
+        jButton21.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton21.setForeground(new java.awt.Color(68, 71, 90));
+        jButton21.setText("H13");
+        jPanel2.add(jButton21);
+
+        jButton6.setBackground(new java.awt.Color(139, 233, 253));
+        jButton6.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton6.setForeground(new java.awt.Color(68, 71, 90));
+        jButton6.setText("H14");
+        jPanel2.add(jButton6);
+
+        jButton12.setBackground(new java.awt.Color(139, 233, 253));
+        jButton12.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton12.setForeground(new java.awt.Color(68, 71, 90));
+        jButton12.setText("H15");
+        jPanel2.add(jButton12);
+
+        jButton15.setBackground(new java.awt.Color(139, 233, 253));
+        jButton15.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton15.setForeground(new java.awt.Color(68, 71, 90));
+        jButton15.setText("H16");
+        jPanel2.add(jButton15);
+
+        jButton16.setBackground(new java.awt.Color(139, 233, 253));
+        jButton16.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton16.setForeground(new java.awt.Color(68, 71, 90));
+        jButton16.setText("H17");
+        jPanel2.add(jButton16);
+
+        jButton17.setBackground(new java.awt.Color(139, 233, 253));
+        jButton17.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton17.setForeground(new java.awt.Color(68, 71, 90));
+        jButton17.setText("H18");
+        jPanel2.add(jButton17);
+
+        H19.setBackground(new java.awt.Color(139, 233, 253));
+        H19.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        H19.setForeground(new java.awt.Color(68, 71, 90));
+        H19.setText("H19");
+        jPanel2.add(H19);
+
+        jButton19.setBackground(new java.awt.Color(139, 233, 253));
+        jButton19.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton19.setForeground(new java.awt.Color(68, 71, 90));
+        jButton19.setText("H20");
+        jPanel2.add(jButton19);
+
+        jButton20.setBackground(new java.awt.Color(139, 233, 253));
+        jButton20.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton20.setForeground(new java.awt.Color(68, 71, 90));
+        jButton20.setText("H21");
+        jPanel2.add(jButton20);
+
+        jButton22.setBackground(new java.awt.Color(139, 233, 253));
+        jButton22.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton22.setForeground(new java.awt.Color(68, 71, 90));
+        jButton22.setText("H22");
+        jPanel2.add(jButton22);
+
+        jButton23.setBackground(new java.awt.Color(139, 233, 253));
+        jButton23.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton23.setForeground(new java.awt.Color(68, 71, 90));
+        jButton23.setText("H23");
+        jPanel2.add(jButton23);
+
+        jButton24.setBackground(new java.awt.Color(139, 233, 253));
+        jButton24.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton24.setForeground(new java.awt.Color(68, 71, 90));
+        jButton24.setText("H24");
+        jPanel2.add(jButton24);
+
+        jButton25.setBackground(new java.awt.Color(139, 233, 253));
+        jButton25.setFont(new java.awt.Font("Roboto", 1, 24)); // NOI18N
+        jButton25.setForeground(new java.awt.Color(68, 71, 90));
+        jButton25.setText("H25");
+        jPanel2.add(jButton25);
+
+        jPanel4.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(40, 20, 560, 550));
+
+        jPanel1.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 100, 640, 670));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -69,6 +267,10 @@ public class FrameRecepcionista extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -106,8 +308,36 @@ public class FrameRecepcionista extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton H19;
+    private javax.swing.JButton jButton1;
+    private javax.swing.JButton jButton10;
+    private javax.swing.JButton jButton11;
+    private javax.swing.JButton jButton12;
+    private javax.swing.JButton jButton13;
+    private javax.swing.JButton jButton14;
+    private javax.swing.JButton jButton15;
+    private javax.swing.JButton jButton16;
+    private javax.swing.JButton jButton17;
+    private javax.swing.JButton jButton19;
+    private javax.swing.JButton jButton2;
+    private javax.swing.JButton jButton20;
+    private javax.swing.JButton jButton21;
+    private javax.swing.JButton jButton22;
+    private javax.swing.JButton jButton23;
+    private javax.swing.JButton jButton24;
+    private javax.swing.JButton jButton25;
+    private javax.swing.JButton jButton3;
+    private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
+    private javax.swing.JButton jButton8;
+    private javax.swing.JButton jButton9;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
+    private javax.swing.JPanel jPanel3;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JPanel panel_habitaciones;
     // End of variables declaration//GEN-END:variables
 }
